92e65328596163b433ddb116054821dc76ba96cc
rachel <repiquero.coc@phinmaed.com>
2026-07-31 09:56:15 +0800

    Third step added

--- hello.py
+++ hello.py
@@ -8,12 +8,3 @@ a = int(input("Enter number 1: "))
 b = int(input("Enter number 2: "))
 print("Sum =", a + b)
 
-name = input("Enter your name: ")
-print("Hello", name)
-    if name != "":
-print("Welcome!")
-
-
-git init
-git add .
-git commit -m "First commit"
